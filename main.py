e90b133f9e7de4032632e5ca46f2465d9ac30764
QuentinAyers1917 <rkowert@posteo.de>
2024-08-19 00:14:28 +0200

    get text to morse conversion to work

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from morse_code import morse_code 
 
-text = input("Type text to convert to morse code: ")
-print(morse_code[text])
+text = list(input("Type text to convert to morse code: "))
+
+morse_code_string = ""
+
+for char in text:
+    morse_code_string += morse_code[char] + " "
+
+print(morse_code_string)
+
