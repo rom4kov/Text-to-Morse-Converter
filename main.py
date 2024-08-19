7d179139f4289de5d400519e32f6f0cf6a7a50ea
QuentinAyers1917 <rkowert@posteo.de>
2024-08-19 15:03:27 +0200

    refactor nested for loop with reverse lookup dictionary and dictionary comprehension

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ from morse_code import morse_code
 
 class MorseConverter():
     def __init__(self) -> None:
-        self.operation = input("Type e to encode text to morse or d to decode morse to text: ")
+        self.operation = ""
         self.op_choice()
         
     def op_choice(self):
+        while self.operation != "e" and self.operation != "d":
+            self.operation = input("Type 'e' to encode text to morse or 'd' to decode morse to text: ")
         if self.operation == "e":
             self.encode()
         elif self.operation == "d":
@@ -21,11 +23,21 @@ class MorseConverter():
                 morse_code_string += "   "
             else:
                 morse_code_string += morse_code[char] + " "
-                pass
         print(morse_code_string)
 
-
     def decode(self):
-        pass
+        reverse_morse_code = {value: key for key, value in morse_code.items()}
 
+        plain_text_string = ""
+        morse_words = input("Please provide morse code to decode to text: ").split("   ")
+
+        for word in morse_words: 
+            morse_letters = word.split()
+                    
+            text_word = ''.join(reverse_morse_code[letter] for letter in morse_letters)
+            plain_text_string += text_word + " "
+
+        print(plain_text_string)
+
+morse_converter = MorseConverter()
 
