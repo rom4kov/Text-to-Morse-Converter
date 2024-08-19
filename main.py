8a4d0049a1573efe5caeacdefcc9f76fbfbe7bc3
QuentinAyers1917 <rkowert@posteo.de>
2024-08-19 10:19:13 +0200

    start implementing class logic

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
 from morse_code import morse_code 
 
-text = list(input("Type text to convert to morse code: ").lower())
 
-morse_code_string = ""
+class MorseConverter():
+    def __init__(self) -> None:
+        self.operation = input("Type e to encode text to morse or d to decode morse to text: ")
+        self.op_choice()
+        
+    def op_choice(self):
+        if self.operation == "e":
+            self.encode()
+        elif self.operation == "d":
+            self.decode()
 
-for char in text:
-    if char == " ":
-        morse_code_string += "   "
-    else:
-        morse_code_string += morse_code[char] + " "
+    def encode(self):
+        morse_code_string = ""
+        text = list(input("Type text to convert to morse code: ").lower())
 
-print(morse_code_string)
+        for char in text:
+            if char == " ":
+                morse_code_string += "   "
+            else:
+                morse_code_string += morse_code[char] + " "
+                pass
+        print(morse_code_string)
+
+
+    def decode(self):
+        pass
+
 
